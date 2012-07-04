7aa123066cd792fa92df99a731cefc435e421ea5
Nick Shvelidze <Nick Shvelidze>
2012-07-04 23:59:11 +0400

    some stuff

--- js/all.js
+++ js/all.js
@@ -10,3 +10,14 @@ a){var b=F.exec(a);b&&(b[1]=(b[1]||"").toLowerCase(),b[3]=b[3]&&new RegExp("(?:^
 $(function(){
 	$.localScroll();
 });
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-31149000-1']);
+  _gaq.push(['_setDomainName', 'nicksh.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
