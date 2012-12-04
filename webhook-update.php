6907949d25a29f908b431be0c43e06c52cc8b4fe
Nick Shvelidze <shveloo@gmail.com>
2012-12-04 17:35:13 +0400

    Update webhook

--- webhook-update.php
+++ webhook-update.php
@@ -1,3 +1,5 @@
 <?php
+ignore_user_abort(true);
+set_time_limit(0);
 system('git pull origin master');
 file_put_contents('../deployment.log',date("m-d-Y H:i").'| Pulling from Github.',FILE_APPEND);
