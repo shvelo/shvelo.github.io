b2faa84d498374d838a3b186e7e4f0af6ccdc842
Nick Shvelidze <shveloo@gmail.com>
2013-01-08 17:25:08 +0400

    Update

--- haml_helpers.rb
+++ haml_helpers.rb
@@ -3,3 +3,24 @@ def if_ie
 		yield
 	end
 end
+
+def meta_charset charset
+	"<meta charset='#{charset}'>"
+end
+
+def meta_description description
+	"<meta name='description' content='#{description}'>"
+end
+
+
+def meta_viewport viewport = "width=device-width,initial-scale=1"
+	"<meta name='viewport' content='#{viewport}'>"
+end
+
+def favicon href
+	"<link rel='shortcut icon' href='#{href}'>"
+end
+
+def stylesheet href
+	"<link rel='stylesheet' href='#{href}'>"
+end
